5ce02e56838d0792c0a90cd2ff05141fbe421ec1
Yurii Husak <gusak.1881@gmail.com>
2018-12-26 16:26:38 +0200

    Stage.js class was updated

--- examples/easyGeometry/1/ui/Stage.js
+++ examples/easyGeometry/1/ui/Stage.js
@@ -1,16 +1,26 @@
 function Stage(){
-	PIXI.Container.call(this)
+	PIXI.Container.call(this);
 
+	this.localStage = null;
+
+	this.isClicked = false;
+	this.isMoved = false;
+
+	this.addAllComponents();
 }
 	
 Stage.prototype = Object.create(PIXI.Container.prototype);
 Stage.prototype.constructor = Stage;
 
+Stage.prototype.addAllComponents = function () {
+	this.localStage = this.addChild(new LocalStage(10,10,30));
+
+}
 
 Stage.prototype.ticker = function(delta) {
 	
 };
 
 Stage.prototype.resizeStage = function(glW, glH) {
-
+	
 };
