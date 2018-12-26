f0dd2af6f1d72bda1b50f158bc48c4c18c428abd
Yurii Husak <gusak.1881@gmail.com>
2018-12-26 17:18:34 +0200

    The localRect member was moved to new Class. The code was refactored a little bit

--- examples/easyGeometry/1/ui/LocalStage.js
+++ examples/easyGeometry/1/ui/LocalStage.js
@@ -8,31 +8,28 @@ function LocalStage(rows, columns, cellSize) {
 	this._w = this.cellSize * this.columns;
 	this._h = this.cellSize * this.rows
 
+	this._txt = this.addChild(Utils.drawText("x: 0\ny: 0", "0x000000"));
+	this._txt.position.set(-this._w/2, -this._h/2);
+	this._txt.style.fontSize = 15;
+
 	this.centerPoint = null;
 	this.localRect = null;
 
-	this.interactive = true;
 	this.isClicked = false;
 	this.isMoved = false;
 
 	this.addGrid();
 
+	//this one for global manipulation
 	this.hitArea = new PIXI.Rectangle(-this._w/2, -this._h/2, this._w, this._h);
-
-	this.localRect.on("pointerdown", this.onMousedown, this);
-	this.localRect.on("pointermove", this.onMousemove, this);
-	this.localRect.on("pointerup", this.onMouseup, this);
-	this.localRect.on("pointerupouside", this.onMouseup, this);
 };
 
 LocalStage.prototype = Object.create(PIXI.Container.prototype);
 LocalStage.prototype.constructor = LocalStage;
 
 LocalStage.prototype.addGrid = function () {
-
-	this.addChild(Utils.drawText("X", 0xFF0000)).position.set(this._w/2 + 20, 0);
-	this.addChild(Utils.drawText("Y", 0xFF0000)).position.set(0, this._h/2 + 20);
-
+	this.addLabes();
+	
 	for (var i = 0; i < this.rows; i++) {
 		for (var j = 0; j < this.columns; j++) {
 
@@ -43,21 +40,39 @@ LocalStage.prototype.addGrid = function () {
 		}
 	}
 
-	this.centerPoint = this.addChild(Utils.drawCircle(0, 0, 5, 0xFF0000, 1));
-	this.localRect = this.addChild(Utils.drawRect(-20,-20, 40, 40, 0x00FFFF, 0.5));
-	this.localRect.interactive = true;
-
-	this._txt = this.localRect.addChild(Utils.drawText("x: 0\ny: 0", "0x000000"));
-	this._txt.style.fontSize = 15;
-
-	this.addChild(Utils.drawLine({x: -this._w/2, y: 0}, {x: this._w/2, y: 0}, 2, 0xFF0000, 1));
-	this.addChild(Utils.drawLine({x: 0, y: -this._h/2}, {x: 0, y: this._h/2}, 2, 0xFF0000, 1));
+	this.addCenterPoint();
+	this.addIneractiveRect();
+	this.addAxes();
 
 };
+
+LocalStage.prototype.addLabes = function () {
+	this.addChild(Utils.drawText("X", 0xFF0000)).position.set(this._w/2 + 20, 0);
+	this.addChild(Utils.drawText("Y", 0xFF0000)).position.set(0, this._h/2 + 20);
+}
+
+LocalStage.prototype.addCenterPoint = function () {
+	this.centerPoint = this.addChild(Utils.drawCircle(0, 0, 5, 0xFF0000, 1));
+}
+
+LocalStage.prototype.addIneractiveRect = function () {
+	this.localRect = this.addChild(new InteractiveRect());
+
+	this.localRect.on("pointerdown", this.onMousedown, this);
+	this.localRect.on("pointermove", this.onMousemove, this);
+	this.localRect.on("pointerup", this.onMouseup, this);
+	this.localRect.on("pointerupouside", this.onMouseup, this);
+}
+
+LocalStage.prototype.addAxes = function () {
+	this.addChild(Utils.drawLine({x: -this._w/2, y: 0}, {x: this._w/2, y: 0}, 2, 0xFF0000, 1));
+	this.addChild(Utils.drawLine({x: 0, y: -this._h/2}, {x: 0, y: this._h/2}, 2, 0xFF0000, 1));
+}
 
 LocalStage.prototype.onMousedown = function (event) {
 	if(this.isClicked){return;}
 	this.isClicked = true;
+	event.stopPropagation();
 };
 
 LocalStage.prototype.onMousemove = function (event) {
@@ -66,7 +81,9 @@ LocalStage.prototype.onMousemove = function (event) {
 
 	var loc = event.data.getLocalPosition(this.centerPoint);
 	this.localRect.position.set(loc.x, loc.y);
-	this._txt.text = "x: " + (loc.x | 0) + "\ny: " + (loc.y | 0);
+	this.localRect._txt.text = "x: " + (loc.x | 0) + "\ny: " + (loc.y | 0);
+
+	event.stopPropagation();
 
 };
 
