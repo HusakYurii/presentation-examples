e70fed76aa3e22c94b687d1a373961c00088d603
Yurii Husak <gusak.1881@gmail.com>
2018-12-26 17:25:33 +0200

    Stage file was refactored

--- examples/easyGeometry/1/ui/Stage.js
+++ examples/easyGeometry/1/ui/Stage.js
@@ -1,7 +1,13 @@
 function Stage(){
 	PIXI.Container.call(this);
 
-	this.localStage = null;
+	this.stagesAmount = 4;
+	this.stages = [];
+
+	this.centerPoint = null;
+
+	this.firstLocalStage = null;
+	this.secondLocalStage = null;
 
 	this.isClicked = false;
 	this.isMoved = false;
@@ -13,9 +19,42 @@ Stage.prototype = Object.create(PIXI.Container.prototype);
 Stage.prototype.constructor = Stage;
 
 Stage.prototype.addAllComponents = function () {
-	this.localStage = this.addChild(new LocalStage(10,10,30));
+	this.centerPoint = this.addChild(Utils.drawCircle(0, 0, 7, 0xFFFF00, 1));
 
+	this.firstLocalStage = this.addChild(new LocalStage(10,10,30));
+
+	this.secondLocalStage = this.addChild(new LocalStage(5,5,30));
+	//Uncoment this one to set the initial positions to the secondLocalStage
+	//this.secondLocalStage.position.set(250,250);
+	this.secondLocalStage.interactive = true;
+
+	this.secondLocalStage.on("pointerdown", this.onStageClick, this);
+	this.secondLocalStage.on("pointermove", this.onStageMove, this);
+	this.secondLocalStage.on("pointerup", this.onStageUp, this);
+	this.secondLocalStage.on("pointerupouside", this.onStageUp, this);
 }
+
+Stage.prototype.onStageClick = function (event) {
+	if(this.isClicked){return;}
+	this.isClicked = true;
+	event.stopPropagation();
+};
+
+Stage.prototype.onStageMove = function (event) {
+	if(!this.isClicked){return;}
+	this.isMoved = true;
+
+	var loc = event.data.getLocalPosition(this);
+	this.secondLocalStage.position.set(loc.x, loc.y);
+	this.secondLocalStage._txt.text = "x: " + (loc.x | 0) + "\ny: " + (loc.y | 0);
+	
+	event.stopPropagation();
+};
+
+Stage.prototype.onStageUp = function (event) {
+	this.isMoved = false;
+	this.isClicked = false;
+};
 
 Stage.prototype.ticker = function(delta) {
 	
