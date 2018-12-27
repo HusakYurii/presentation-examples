38a0d015adbbfa9f9f191f60acf168fe4d0b2bf0
Yurii Husak <gusak.1881@gmail.com>
2018-12-27 11:00:08 +0200

    InteractiveRect class was refactored. Now all related interactivity functions are in this class

--- examples/easyGeometry/1/ui/InteractiveRect.js
+++ examples/easyGeometry/1/ui/InteractiveRect.js
@@ -3,10 +3,42 @@ function InteractiveRect() {
 
 	this.localRect = this.addChild(Utils.drawRect(-20,-20, 40, 40, 0x00FFFF, 0.5));
 	this.interactive = true;
+	this.isClicked = false;
+	this.isMoved = false;
 
 	this._txt = this.addChild(Utils.drawText("x: 0\ny: 0", "0x000000"));
 	this._txt.style.fontSize = 15;
+
+
+	this.on("pointerdown", this.onMousedown, this);
+	this.on("pointermove", this.onMousemove, this);
+	this.on("pointerup", this.onMouseup, this);
+	this.on("pointerupouside", this.onMouseup, this);
 };
 
 InteractiveRect.prototype = Object.create(PIXI.Container.prototype);
-InteractiveRect.prototype.constructor = Stage;
+InteractiveRect.prototype.constructor = InteractiveRect;
+
+
+InteractiveRect.prototype.onMousedown = function (event) {
+	if(this.isClicked){return;}
+	this.isClicked = true;
+	event.stopPropagation();
+};
+
+InteractiveRect.prototype.onMousemove = function (event) {
+	if(!this.isClicked){return;}
+	this.isMoved = true;
+
+	var loc = event.data.getLocalPosition(this.parent);
+	this.position.set(loc.x, loc.y);
+	this._txt.text = "x: " + (loc.x | 0) + "\ny: " + (loc.y | 0);
+
+	event.stopPropagation();
+
+};
+
+InteractiveRect.prototype.onMouseup = function (event) {
+	this.isMoved = false;
+	this.isClicked = false;
+};
