b8d154dce964f1b656443bbaaf161bbcd2603818
Yurii <gmail.1881@gmail.com>
2019-01-08 14:22:08 +0200

    Stage.js refactored

--- examples/easyGeometry/2/ui/Stage.js
+++ examples/easyGeometry/2/ui/Stage.js
@@ -1,6 +1,12 @@
 function Stage(){
 	PIXI.Container.call(this);
-	
+
+	this.outerStage = null;
+	this.outerBall = null;
+
+	this.innerStage = null;
+	this.innerBall = null;
+
 
 	this.addAllComponents();
 }
@@ -9,10 +15,16 @@ Stage.prototype = Object.create(PIXI.Container.prototype);
 Stage.prototype.constructor = Stage;
 
 Stage.prototype.addAllComponents = function () {
+    this.outerStage = this.addChild(new Axes(400, 400));
+    this.outerBall = this.outerStage.addChild(new Ball(this.outerStage, 20, 1));
 
+    this.innerStage = this.addChild(new Axes(250, 250, 300, -150));
+    this.innerBall = this.innerStage.addChild(new Ball(this.innerStage, 10, -0.5));
 };
-Stage.prototype.ticker = function(delta) {
 
+Stage.prototype.ticker = function(delta) {
+	if(this.outerBall){this.outerBall.ticker(delta);}
+	if(this.innerBall){this.innerBall.ticker(delta);}
 };
 
 Stage.prototype.resizeStage = function(glW, glH) {
