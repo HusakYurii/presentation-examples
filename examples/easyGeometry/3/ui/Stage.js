1b4f3ed5c9cac92c249627db92a1a22c0168b75c
Yurii <gmail.1881@gmail.com>
2019-01-08 15:41:15 +0200

    stage code refactoring was done

--- examples/easyGeometry/3/ui/Stage.js
+++ examples/easyGeometry/3/ui/Stage.js
@@ -2,12 +2,14 @@ function Stage(){
 	PIXI.Container.call(this);
 	
 	this.arrowsContainer = null;
+	this.firstStage = null;
+	this.secondStage = null;
 
 	this.isAnimated = false;
 
 	this._speed = 20;
 	this._dist = 0;
-	this.animationDistance = 0;
+	this.animationDistance = 1000;
 
 
 	this.addAllComponents();
@@ -21,28 +23,30 @@ Stage.prototype.addAllComponents = function () {
 	this.arrowsContainer.scale.set(0.15);
 	this.arrowsContainer.on("left", this.scrollScages, this);
 	this.arrowsContainer.on("right", this.scrollScages, this);
-}
 
-Stage.prototype.addArrows = function () {
+	this.firstStage = this.addChild(new FirstStage(600, 500, 0,0));
+	this.secondStage = this.addChild(new FirstStage(300, 500, 1000,0));
 
-}
+};
+
 
 Stage.prototype.scrollScages = function (dir) {
 	if(this.isAnimated){return;}
 	this.isAnimated = true;
 	this._dir = dir;
-}
+};
 
 Stage.prototype.ticker = function(delta) {
 	if(!this.isAnimated){return;}
 
-	// this._dist += this._speed;
-	// if(this._dist >= this.animationDistance){
-	// 	this.isAnimated = false;
-	// 	this._dist = 0;
-	// }
+	this._dist += this._speed;
+	if(this._dist >= this.animationDistance){
+		this.isAnimated = false;
+		this._dist = 0;
+	}
 
-	// this.someExample.move(this._speed * this._dir, 0);
+	this.firstStage.move(this._speed * this._dir, 0);
+    this.secondStage.move(this._speed * this._dir, 0);
 };
 
 Stage.prototype.resizeStage = function(glW, glH) {
