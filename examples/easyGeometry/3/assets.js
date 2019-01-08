a1d60fd90569b3ac294a8c251d06f76d8b34b9ce
Yurii <gmail.1881@gmail.com>
2019-01-08 15:36:34 +0200

    assets file was updated

--- examples/easyGeometry/3/assets.js
+++ examples/easyGeometry/3/assets.js
@@ -2,5 +2,13 @@ var Assets = [
 	{
 		name: "arrow",
 		url: "./img/arrow.png"
-	}	
+	},
+    {
+        name: "base",
+        url: "./img/base.png"
+    },
+    {
+        name: "muzzle",
+        url: "./img/muzzle.png"
+    }
 ];
